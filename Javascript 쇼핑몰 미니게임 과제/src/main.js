316cbc4bde59ed80f703f0a897d52d8e1c9b3aca
gardening-y <60008786@naver.com>
2023-01-04 06:27:54 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -22,11 +22,57 @@ function createHTMLString(item) {
   `;
 }
 
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if(key == null || value == null) {
+    return;
+  }
+
+  const filtered = items.filter(item => item[key] === value);
+  displayItems(filtered);
+}
+
+// // Handle button click
+// function onButtonClick(event, items) {
+//   const target = event.target;
+//   const key = target.dataset.key;
+//   const value = dataset.value;
+
+//   if(key == null || value == null) {
+//     return;
+//   }
+  
+//   updateItems(items, key, value);
+// }
+
+// // Make the items matching {key: value} invisible.
+// function updateItems(items, key, value) {
+//   items.forEach(item => {
+//     if (item.dataset[key] === value) {
+//       item.classList.remove('invisible');
+//     }
+//     else {
+//       item.classList.add('invisible');
+//     }
+//   });
+// }
+
+function setEventListener(items) {
+  const logo = document.querySelector('.logo');
+  //이벤트 위임
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 // main
 loadItems() 
   .then(items => {
     console.log(items);
     displayItems(items);
-    // setEventListener(items);
+    setEventListener(items);
   })
   .catch(console.log);
