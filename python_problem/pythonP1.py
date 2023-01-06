5441251800e61501d54270af37b75a059f0bdf7c
gardening-y <60008786@naver.com>
2023-01-07 03:12:58 +0900

    python1-5단계

--- python_problem/pythonP1.py
+++ python_problem/pythonP1.py
@@ -1,23 +1,31 @@
 num = 0
 
-input_num = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+def print_play(input_num, name):
+  global num
+  for i in range(int(input_num)):
+    print(name, ':', num+1+i)
+  num += int(input_num)
 
-def print_play(input_num):
-  for i in range(num, num+int(input_num)):
-    print('playerA :', i+1)
+def speak_num(name):
+  input_num = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
-while True:
-  if input_num.isdigit():
-    if input_num == '1':
-      print_play(input_num)
-      break
-    elif input_num == '2':
-      print_play(input_num)
-      break
-    elif input_num == '3':
-      print_play(input_num)
-      break
+  while True:
+    if input_num.isdigit():
+      if input_num == '1':
+        print_play(input_num, name)
+        break
+      elif input_num == '2':
+        print_play(input_num, name)
+        break
+      elif input_num == '3':
+        print_play(input_num, name)
+        break
+      else:
+        input_num = input('1,2,3 중 하나를 입력하세요')
     else:
-      input_num = input('1,2,3 중 하나를 입력하세요')
-  else:
-    input_num = input('정수를 입력하세요')
+      input_num = input('정수를 입력하세요')
+
+
+speak_num('playerA')
+speak_num('playerB')
+
