9d41ec1533f4e800e80a7f00bd188985a8670389
gardening-y <60008786@naver.com>
2023-01-07 03:51:34 +0900

    python1-9단계

--- python_problem/pythonP1.py
+++ python_problem/pythonP1.py
@@ -1,3 +1,4 @@
+import random
 num = 0
 
 def print_play(input_num, name):
@@ -9,23 +10,27 @@ def print_play(input_num, name):
       break
 
 def speak_num(name):
-  input_num = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+  if name == 'computer':
+    input_num = random.randint(1,3)
+    print_play(input_num, name)
+  else: 
+    input_num = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
-  while True:
-    if input_num.isdigit():
-      if input_num == '1':
-        print_play(input_num, name)
-        break
-      elif input_num == '2':
-        print_play(input_num, name)
-        break
-      elif input_num == '3':
-        print_play(input_num, name)
-        break
+    while True:
+      if input_num.isdigit():
+        if input_num == '1':
+          print_play(input_num, name)
+          break
+        elif input_num == '2':
+          print_play(input_num, name)
+          break
+        elif input_num == '3':
+          print_play(input_num, name)
+          break
+        else:
+          input_num = input('1,2,3 중 하나를 입력하세요')
       else:
-        input_num = input('1,2,3 중 하나를 입력하세요')
-    else:
-      input_num = input('정수를 입력하세요')
+        input_num = input('정수를 입력하세요')
 
 def brGame(name1, name2):
   while True:
@@ -38,4 +43,4 @@ def brGame(name1, name2):
       print(name1, 'win!')
       break
 
-brGame('playerA', 'playerB')
+brGame('computer', 'player')
